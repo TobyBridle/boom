aa9dcfeb5c7cbad92fad25c0d357dbe4d50b5f4f
Toby Bridle <foreignspaghettibolognese@gmail.com>
2025-03-26 00:16:55 +0000

    feat(template): remove vec to reduce allocs to 0

--- tests/template.rs
+++ tests/template.rs
@@ -4,23 +4,52 @@
 ))]
 use std::arch::x86_64::*;
 
+use std::ptr;
 use std::time::Instant;
 
+const MAX_TEMPLATE_TERMS: usize = 2;
+
 #[inline(always)]
-fn iterative_parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
-    let mut results = Vec::new();
+fn merge_slices(a: &mut [(usize, usize)], b: &[(usize, usize)]) {
+    let mut a_idx = 0usize;
+    let mut b_idx = 0usize;
+
+    let a_len = a.len();
+    let b_len = b.len();
+
+    unsafe {
+        while a_idx < a_len && b_idx < b_len {
+            let a_ptr = a.as_mut_ptr().add(a_idx);
+            let b_val = *b.get_unchecked(b_idx);
+
+            if *a_ptr == (0, 0) {
+                ptr::write(a_ptr, b_val);
+                b_idx += 1;
+            }
+            a_idx += 1;
+        }
+    }
+}
+
+#[inline(always)]
+fn iterative_parse_template_indexes(
+    template: &str,
+) -> Option<[(usize, usize); MAX_TEMPLATE_TERMS]> {
+    let mut results = [(0usize, 0usize); MAX_TEMPLATE_TERMS];
+    let mut idx = 0usize;
     let mut slice_index = 0;
 
     while let Some(start_index) = template[slice_index..].find("{{{s}}}") {
         let start = start_index + slice_index;
         let end = (start + "{{{s}}}".len()).min(template.len());
-        results.push((start, end));
+        results[idx] = (start, end);
 
         // Move the slice_index forward to continue searching after the current match
         slice_index = end;
+        idx += 1;
     }
 
-    if results.is_empty() {
+    if results[0] == (0, 0) {
         None
     } else {
         Some(results)
@@ -28,7 +57,7 @@ fn iterative_parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)
 }
 
 #[inline(always)]
-fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
+fn parse_template_indexes(template: &str) -> Option<[(usize, usize); MAX_TEMPLATE_TERMS]> {
     #[cfg(all(
         any(target_arch = "x86", target_arch = "x86_64"),
         target_feature = "avx2"
@@ -41,7 +70,8 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
 
             let mask = unsafe { _mm_set1_epi8('{' as i8) };
 
-            let mut results: Vec<(usize, usize)> = Vec::new();
+            let mut results = [(0usize, 0usize); MAX_TEMPLATE_TERMS];
+            let mut res_idx = 0usize;
 
             for chunk in aligned {
                 let simd_chunk = unsafe { _mm_load_si128(chunk.as_ptr() as *const __m128i) };
@@ -51,7 +81,8 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
                 if mask_chunk != 0 {
                     let idx: usize = offset + mask_chunk.trailing_zeros() as usize;
                     if &template[idx..idx + "{{{s}}}".len()] == "{{{s}}}" {
-                        results.push((idx, idx + 7))
+                        results[res_idx] = (idx, idx + 7);
+                        res_idx += 1;
                     }
                 }
 
@@ -62,15 +93,19 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
                 &template[template.len() - suffix.len()..],
             )
             .map(|offset_pairs| {
-                offset_pairs
-                    .iter()
-                    .map(|(start, end)| (offset + start, offset + end))
-                    .collect::<Vec<(usize, usize)>>()
+                let mut result = [(0, 0); MAX_TEMPLATE_TERMS]; // Preallocate stack memory
+                for (i, (start, end)) in offset_pairs.iter().enumerate().take(MAX_TEMPLATE_TERMS) {
+                    if *end == 0 {
+                        continue;
+                    }
+                    result[i] = (offset + start, offset + end);
+                }
+                result
             }) {
-                results.extend(suffix_pairs);
+                merge_slices(&mut results, &suffix_pairs);
             }
 
-            if results.is_empty() {
+            if results[0] == (0, 0) {
                 None
             } else {
                 Some(results)
@@ -82,7 +117,8 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
 
             let mask = unsafe { _mm256_set1_epi8('{' as i8) };
 
-            let mut results: Vec<(usize, usize)> = Vec::new();
+            let mut results = [(0usize, 0usize); MAX_TEMPLATE_TERMS];
+            let mut res_idx = 0usize;
 
             for chunk in aligned {
                 let simd_chunk = unsafe { _mm256_load_si256(chunk.as_ptr() as *const __m256i) };
@@ -92,7 +128,8 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
                 if mask_chunk != 0 {
                     let idx: usize = offset + mask_chunk.trailing_zeros() as usize;
                     if &template[idx..idx + "{{{s}}}".len()] == "{{{s}}}" {
-                        results.push((idx, idx + 7))
+                        results[res_idx] = (idx, idx + 7);
+                        res_idx += 1;
                     }
                 }
 
@@ -103,15 +140,19 @@ fn parse_template_indexes(template: &str) -> Option<Vec<(usize, usize)>> {
                 &template[template.len() - suffix.len()..],
             )
             .map(|offset_pairs| {
-                offset_pairs
-                    .iter()
-                    .map(|(start, end)| (offset + start, offset + end))
-                    .collect::<Vec<(usize, usize)>>()
+                let mut result = [(0, 0); MAX_TEMPLATE_TERMS]; // Preallocate stack memory
+                for (i, (start, end)) in offset_pairs.iter().enumerate().take(MAX_TEMPLATE_TERMS) {
+                    if *end == 0 {
+                        continue;
+                    }
+                    result[i] = (offset + start, offset + end);
+                }
+                result
             }) {
-                results.extend(suffix_pairs);
+                merge_slices(&mut results, &suffix_pairs);
             }
 
-            if results.is_empty() {
+            if results[0] == (0, 0) {
                 None
             } else {
                 Some(results)
@@ -142,7 +183,7 @@ fn test_template_suffix() {
     );
     assert_eq!(
         indices,
-        Some(vec![(template.len() - "{{{s}}}".len(), template.len())])
+        Some([(template.len() - "{{{s}}}".len(), template.len()), (0, 0)])
     );
 }
 
@@ -157,7 +198,7 @@ fn test_template_suffix_long() {
     );
     assert_eq!(
         indices,
-        Some(vec![(template.len() - "{{{s}}}".len(), template.len())])
+        Some([(template.len() - "{{{s}}}".len(), template.len()), (0, 0)])
     );
 }
 
@@ -167,7 +208,7 @@ fn test_template_infix() {
     let timer = Instant::now();
     let indices = parse_template_indexes(template);
     eprintln!("Took {:?} to get template indices (INFIX)", timer.elapsed());
-    assert_eq!(indices, Some(vec![(30, 37)]));
+    assert_eq!(indices, Some([(30, 37), (0, 0)]));
 }
 
 #[test]
@@ -179,7 +220,7 @@ fn test_template_infix_long() {
         "Took {:?} to get template indices (INFIX LONG)",
         timer.elapsed()
     );
-    assert_eq!(indices, Some(vec![(79, 86)]));
+    assert_eq!(indices, Some([(79, 86), (0, 0)]));
 }
 
 #[test]
@@ -191,7 +232,9 @@ fn test_template_infix_multiple() {
         "Took {:?} to get template indices (INFIX LONG MULTIPLE)",
         timer.elapsed()
     );
-    assert_eq!(indices, Some(vec![(30, 37), (78, 85)]));
+    assert_eq!(indices, Some([(30, 37), (78, 85)]));
+}
+
 #[cfg(feature = "measure-allocs")]
 mod tests {
     use super::*;
