027c04ff14cb953c1687f33440220ea0973fa96b
Toby Bridle <foreignspaghettibolognese@gmail.com>
2026-02-27 23:21:05 +0000

    feat(api): update user config with new bangs

--- boom-web/src/routes/api/add_bang.rs
+++ boom-web/src/routes/api/add_bang.rs
@@ -1,4 +1,5 @@
-use axum::{Json, http::Response, response::IntoResponse};
+use axum::{Json, extract::State, http::Response, response::IntoResponse};
+use boom_config::{BangCustomConfig, ConfigBuilder};
 use boom_core::{
     Redirect,
     cache::{get_bang, get_redirects, update_redirect},
@@ -7,29 +8,46 @@ use reqwest::StatusCode;
 use serde::Deserialize;
 use serde_json::json;
 
+use crate::AppState;
+
 #[derive(Deserialize, Clone, Debug)]
-pub(crate) struct RedirectExtras {
+pub struct RedirectExtras {
     #[serde(flatten)]
     base: Redirect,
     #[serde(rename = "overwrite")]
     allow_overwrite: Option<String>,
 }
 
-pub async fn add_bang(Json(req): Json<RedirectExtras>) -> impl IntoResponse {
+pub async fn add_bang(
+    State(state): State<AppState>,
+    Json(req): Json<RedirectExtras>,
+) -> impl IntoResponse {
     let new_bang = req.base;
 
     let handle_update_redirect = |b| match update_redirect(b) {
-        Ok(()) => Response::builder()
-            .status(StatusCode::CREATED)
-            .body(
-                json!({
-                    "s": new_bang.short_name,
-                    "t": new_bang.trigger,
-                    "u": new_bang.url_template,
-                })
-                .to_string(),
-            )
-            .unwrap(),
+        Ok(()) => {
+            let mut cfg_builder: ConfigBuilder = state.shared_config.read().unwrap().clone().into();
+            cfg_builder.add_custom_bang(
+                b.trigger.clone(),
+                BangCustomConfig {
+                    template: b.url_template.clone(),
+                    short_name: b.short_name.clone(),
+                },
+            );
+            cfg_builder.serialize();
+
+            Response::builder()
+                .status(StatusCode::CREATED)
+                .body(
+                    json!({
+                        "s": new_bang.short_name,
+                        "t": new_bang.trigger,
+                        "u": new_bang.url_template,
+                    })
+                    .to_string(),
+                )
+                .unwrap()
+        }
         Err(_) => Response::builder()
             .status(StatusCode::INTERNAL_SERVER_ERROR)
             .body(
@@ -41,23 +59,22 @@ pub async fn add_bang(Json(req): Json<RedirectExtras>) -> impl IntoResponse {
 
     match req.allow_overwrite {
         Some(_) => handle_update_redirect(&new_bang),
-        None => {
-            if let Some(b_idx) = get_bang(&new_bang.trigger).unwrap_or_default() {
+        None => get_bang(&new_bang.trigger).unwrap_or_default().map_or_else(
+            || handle_update_redirect(&new_bang),
+            |idx| {
                 let bang = get_redirects().expect("Read Lock on Redirects");
                 Response::builder()
                     .status(StatusCode::CONFLICT)
                     .body(
                         json!({
-                            "s": bang[b_idx].short_name,
-                            "t": bang[b_idx].trigger,
-                            "u": bang[b_idx].url_template,
+                            "s": bang[idx].short_name,
+                            "t": bang[idx].trigger,
+                            "u": bang[idx].url_template,
                         })
                         .to_string(),
                     )
                     .unwrap()
-            } else {
-                handle_update_redirect(&new_bang)
-            }
-        }
+            },
+        ),
     }
 }
